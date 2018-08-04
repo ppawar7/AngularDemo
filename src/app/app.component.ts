4dc85f36257bad076b4def56560d770c301ea367
PUNAM\Home <punampawar23@gmail.com>
2018-08-04 14:45:24 +0530

    simple trial changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'demo-app';
+  enabled:boolean=false;
+  showColor:boolean=true;
+  numberList:number[]=[1,2,3];
+  sampleText:string;
+
+  onInputChange(event):void {
+console.log(event.target.value);
+  };
+  calledMe(inputVal:string):void{
+alert("Hi"+inputVal);
+  };
 }
